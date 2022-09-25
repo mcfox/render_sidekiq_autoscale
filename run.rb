10b36aef5bf74bf88d30ac05f5b7424effc73b74
joselopes.neto <PARTLY-domed-elves>
2022-09-24 22:21:47 -0300

    autoscale sem o rails

--- run.rb
+++ run.rb
@@ -2,15 +2,12 @@ require './app/render_auto_scale'
 
 begin
   puts "Creating worker configs"
-  api_token = 'rnd_M57RvdKsIGwYoGBf8FpmnzVA0ku2'
-  apps = []
-  apps << RenderAutoScale.new('worker','redis://red-cbbvt9cgqg4cu6m01b70:6379', api_token)
-  apps << RenderAutoScale.new('fact-worker','redis://red-cbtbsd319n0c5dqruk40:6379', api_token)
-  puts "Running services"
-  apps.each do |app|
-    puts "Adjusting #{app.worker_service_name}..."
-    app.adjust_workers
-  end
+  service_name = ENV['AUTOSCALE_SERVICE_NAME']
+  redis_url = ENV['REDIS_URL']
+  api_token = ENV['SIDEKIQ_RENDER_AUTOSCALE_API_TOKEN']
+  app = RenderAutoScale.new(service_name, redis_url, api_token)
+  puts "Adjusting #{app.worker_service_name}..."
+  app.adjust_workers
   puts "Done."
 rescue => e
   puts e.message
