d71310cfecbb39af700f9b95f550d2a2ea817138
joseneto <jose.lopes.santos.neto@gmail.com>
2024-02-19 14:52:15 -0300

    Ajustado para ser usado com Rec2

--- run.rb
+++ run.rb
@@ -1,7 +1,7 @@
 require './app/render_auto_scale'
 
 begin
-  puts "Creating worker configs"
+  puts "Starting auto scale service..."
   service_name = ENV['AUTOSCALE_SERVICE_NAME']
   redis_url = ENV['REDIS_URL']
   api_token = ENV['SIDEKIQ_RENDER_AUTOSCALE_API_TOKEN']
@@ -9,6 +9,4 @@ begin
   puts "Adjusting #{app.worker_service_name}..."
   app.adjust_workers
   puts "Done."
-# rescue => e
-#   puts e.message
 end
