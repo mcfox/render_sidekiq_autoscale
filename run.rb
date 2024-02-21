54648194ae9a4c3922eeebfebe522d2a55eb6463
joseneto <jose.lopes.santos.neto@gmail.com>
2024-02-21 11:25:53 -0300

    Testando nova api para jobs agendados porque nem sempre está retornando corretamente

--- run.rb
+++ run.rb
@@ -1,14 +1,13 @@
 require './app/render_auto_scale'
 
 begin
-  puts "Creating worker configs"
+  puts "Starting auto scale service..."
   service_name = ENV['AUTOSCALE_SERVICE_NAME']
   redis_url = ENV['REDIS_URL']
   api_token = ENV['SIDEKIQ_RENDER_AUTOSCALE_API_TOKEN']
   app = RenderAutoScale.new(service_name, redis_url, api_token)
-  puts "Adjusting #{app.worker_service_name}..."
+  puts "service_name: #{service_name}"
+  puts "redis_url: #{redis_url}"
   app.adjust_workers
   puts "Done."
-# rescue => e
-#   puts e.message
 end
